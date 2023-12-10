217220bf4e40f228590b5b4edfddc2ebf6c6876a
WinterAtne <Winter54b@gmail.com>
2023-12-09 23:30:18 -0700

    Rebase

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,9 @@
 #include <string>
 
 
-#include "Dictionary.hpp"
-
-// int main() {
-//     std::cout << "Hello World" << std::endl;
-//     return 0;
-// }
-
 
 int main() {
-    Dictionary dict = Dictionary();
-    dict.Insert("bungus");
-    dict.Insert("bun");
-    dict.Insert("a");
-    dict.Insert("an");
-    dict.Insert("and");
-    dict.Insert("anderson");
-
-
-
-    std::vector<Lexeme*> lss = dict.ReadDictionary(dict.root);
-
-    for (auto lex : lss) {
-        std::cout << lex->word << std::endl;
-    }
+    std::cout << "Hello World" << std::endl;
 
     return 0;
 }
