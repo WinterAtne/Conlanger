537800ad104b2486e890f3b20a6b63cfd3908e9b
WinterAtne <Winter54b@gmail.com>
2023-12-10 01:01:01 -0700

    Created Basic control loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <string>
-
+#include <sqlite3.h>
 
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    bool program_should_close = false;
 
+    sqlite3* DataBase; 
+    int exit = 0; 
+    exit = sqlite3_open("languages.db", &DataBase); 
+
+    
+    while(!program_should_close) {
+        //If we error out sql, we should probably just close the program and return an error
+        if (exit != SQLITE_OK) {
+            program_should_close = true;
+            std::cout << "An Error has occured in SQLite; it is possible your database has been corrupted, or your command was invalid and the Conlanger Parser simply failled to realize. The program will now close automatically" << std::endl;
+            sqlite3_close(DataBase); 
+            return -1;
+        }
+
+        
+    }
+
+
+    sqlite3_close(DataBase); 
     return 0;
 }
