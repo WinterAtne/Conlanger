eeec418b44b31fbf3ca78c156b44c225927d57dc
WinterAtne <Winter54b@gmail.com>
2023-12-11 16:49:33 -0700

    Began formatting commands

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,7 +1,6 @@
 #include "Parser.hpp"
 
 
-
 std::vector<std::string> Parser::SplitCommand(const std::string input) {
     std::vector<std::string> rtrn;
     std::string buffer;
@@ -24,8 +23,20 @@ std::vector<std::string> Parser::SplitCommand(const std::string input) {
 
 
 int Parser::Parse(const std::string input, const sqlite3* DataBase) {
-    std::vector<std::string> broke = SplitCommand(input);
-    
+    std::vector<std::string> splitInput = SplitCommand(input);
+
+    if ("Create" == splitInput[0]) {
+        std::vector<std::string> parents(splitInput.begin() + 2, splitInput.end());
+        Executer::Create(splitInput[1], parents);
+    } else if ("Add" == splitInput[0]) {
+        Executer::Add(splitInput[1], splitInput[2], splitInput[3]);
+    } else if ("Remove" == splitInput[0]) {
+        Executer::Remove(splitInput[1], splitInput[2], splitInput[3]);
+    } else if ("Fetch" == splitInput[0]) {
+        Executer::Fetch(splitInput[1], splitInput[2], splitInput[3]);
+    } else {
+        return 400;
+    }
 
     return 200;
 }
