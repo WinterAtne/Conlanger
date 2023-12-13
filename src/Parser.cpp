ef3c9c12242aa8a78a75b16792b4bfe773a336d6
WinterAtne <Winter54b@gmail.com>
2023-12-12 20:41:31 -0700

    using quotes now  groups words with spaces
    The old SplitCommand function didn't really enable a lot of functionality, namely using spaces. Now it will!

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -5,15 +5,29 @@ std::vector<std::string> Parser::SplitCommand(const std::string input) {
     std::vector<std::string> rtrn;
     std::string buffer;
 
+    bool ignoreSpaces = false;
+    bool controlCharacter = false;
+
     for (auto c : input) {
-        if (' ' == c) {
-            rtrn.push_back(buffer);
-            buffer.clear();
-            continue;
-        } else {
-            buffer = buffer + c;
+        // if we aren't ignoring spaces check for them, then check if we should ignore spaces
+        if (!ignoreSpaces) {
+            if (' ' == c) {
+                rtrn.push_back(buffer);
+                buffer.clear();
+                continue;
+            } else if ('\"' == c) {
+                ignoreSpaces = true;
+                continue;
+            }
+        }
+
+        if ('\"' == c) {
+            ignoreSpaces = false;
             continue;
         }
+
+        // if none of the control characters are found, add this to the buffer.
+        buffer = buffer + c;
     }
 
     rtrn.push_back(buffer);
