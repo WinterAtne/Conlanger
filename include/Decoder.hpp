7afc54b66f3e6cf519386d239a2269ebc2824c55
WinterAtne <Winter54b@gmail.com>
2023-12-14 18:07:10 -0700

    Improved Efficiency of code
    Also fixed the capitalization bug.

--- include/Decoder.hpp
+++ include/Decoder.hpp
@@ -9,9 +9,8 @@ using func=std::function<int(std::vector<std::string>)>;
 class Decoder {
 private:
     static std::map<std::string, func> stringToFunction;
-    static std::vector<std::string> validFunctions;
 public:
     static void AddFunction(std::string str, func fun);
     static func GetFunction(std::string str);
-    static std::vector<std::string> GetFunctionKeys();
+    static bool ValidateKey(std::string str);
 };
